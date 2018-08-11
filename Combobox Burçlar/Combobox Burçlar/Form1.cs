8f1b688b1521549fe7f8c636a1bbc7bd35e78c9c
bbg <bagbrc@gmail.com>
2018-08-11 15:37:00 +0300

    Combobox Burçlar uygulaması eklendı

---
+++ Combobox Burçlar/Combobox Burçlar/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Combobox_Burçlar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = comboBox1.Text;
+            pictureBox1.ImageLocation = "D:/PROJELER/C#/Combobox Burçlar/Combobox Burçlar/bin/balik.jpg";
+        }
+    }
+}
