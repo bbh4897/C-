5db935a2cfb51dd77c0ba04d85d264e3aaa29f24
bbg <bagbrc@gmail.com>
2018-08-11 15:01:48 +0300

    iki yeni program eklendi

---
+++ İf Yapısı/İf Yapısı/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace İf_Yapısı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi;
+
+            sayi = Convert.ToInt32(textBox1.Text);
+            if(sayi < 10)
+            {
+                label2.Text = "Sayı 10'dan küçüktür";
+            }
+            else
+            {
+                label2.Text = "Sayı 10'dan büyüktür";
+            }
+        }
+    }
+}
