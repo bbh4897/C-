f2ad451961b6c0ff880e145e367a5bde35cbdc63
bbg <bagbrc@gmail.com>
2018-08-11 15:47:49 +0300

    Hata yakalama uygulaması

---
+++ Hata Yakalama/Hata Yakalama/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hata_Yakalama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, bolum;
+            try
+            {
+                sayi1 = Convert.ToInt32(textBox1.Text);
+                sayi2 = Convert.ToInt32(textBox2.Text);
+                bolum = sayi1 / sayi2;
+                label3.Text = Convert.ToString(bolum);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+        }
+    }
+}
