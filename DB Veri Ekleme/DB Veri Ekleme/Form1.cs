599ae1578dee356da59f632b8b47b9417bd64957
bbg <bagbrc@gmail.com>
2018-08-12 19:47:02 +0300

    DB Veri Ekleme uygulaması eklendı

---
+++ DB Veri Ekleme/DB Veri Ekleme/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DB_Veri_Ekleme
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection con = new SqlConnection("Data Source=BAGBRC;Initial Catalog=ogrenciler;Integrated Security=True");
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand sc = new SqlCommand("insert into bilgiler(adsoyad, sehir) values('"+textBox1.Text.ToString()+ "','"+textBox2.Text.ToString()+"')",con);
+            sc.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Kayıt Başarıyla Gerçekleşti");
+            this.Hide();
+        }
+    }
+}
