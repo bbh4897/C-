81f587e0057293317140000236620bf5aaebb814
bbg <bagbrc@gmail.com>
2018-08-12 19:06:49 +0300

    DB Listeleme uygulaması eklendi

---
+++ DB Listeleme/DB Listeleme/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DB_Listeleme
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection con = new SqlConnection("Data Source=BAGBRC;Initial Catalog=ogrenciler;Integrated Security=True");
+
+        private void veriGoster()
+        {
+            con.Open();
+            SqlCommand komut = new SqlCommand("select * from bilgiler", con);
+            SqlDataReader rd = komut.ExecuteReader();
+
+            while (rd.Read())
+            {
+                ListViewItem lv = new ListViewItem();
+                lv.Text = rd["id"].ToString();
+                lv.SubItems.Add(rd["adsoyad"].ToString());
+                lv.SubItems.Add(rd["sehir"].ToString());
+                listView1.Items.Add(lv);
+            }
+            con.Close();
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            veriGoster();
+        }
+    }
+}
---
+++ DB Listeleme/DB Listeleme/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("DB Listeleme")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DB Listeleme")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("6e58be8e-3e59-47d8-961e-6a074549b85c")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// '*' kullanarak varsayılana ayarlayabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
