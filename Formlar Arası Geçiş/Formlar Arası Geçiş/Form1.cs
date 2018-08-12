da140aa7ffa133c4085dbe19fda9716c91761d8a
bbg <bagbrc@gmail.com>
2018-08-12 17:47:55 +0300

    Formlar Arası Geçiş Uygulaması Yapıldı

---
+++ Formlar Arası Geçiş/Formlar Arası Geçiş/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formlar_Arası_Geçiş
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("Alanlar Boş Geçilemez!");
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+
+            if (textBox1.Text == "Admin" && textBox2.Text == "123456")
+            {
+                MessageBox.Show("Giriş Başarılı. İkinci Sayfaya Yönlendiriliyorsunuz.");
+                Form2 form2 = new Form2();
+                form2.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Giriş Başarısız. Bilgilerinizi Kontrol Ediniz.");
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+
+
+            
+            
+        }
+    }
+}
---
+++ Formlar Arası Geçiş/Formlar Arası Geçiş/Form2.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Formlar_Arası_Geçiş
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(62, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İkinci Forma Hoş Geldiniz";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 249);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
